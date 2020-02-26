f9a854e806276326970246d68509ea53546d7f1a
RagavendraKM <raghurkm7@gmail.com>
2020-02-27 00:42:59 +0530

    3 files modified, 2 added

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
 import {Geolocation} from '@ionic-native/geolocation/ngx';
 import { LocationServiceService } from '../location-service.service';
+import { ContactPage } from '../contact/contact';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -40,6 +42,7 @@ export class HomePage {
       async res => this.singleRestaurant = await res,
       err => err
     )
+    this.navCtrl.push(ContactPage);
   }
 
   async sendLocDetails(lat,lon){
